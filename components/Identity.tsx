1da2501661ed4e79cef7b4d33ff390b5dbc8ea61
Shun Kakinoki <shunkakinoki@gmail.com>
2022-11-05 01:27:59 -0700

    feat: 🎸 ini lens handle

--- components/Identity.tsx
+++ components/Identity.tsx
@@ -1,6 +1,12 @@
 import { ConnectKitButton } from "connectkit";
-import { IdentityLens } from "./IdentityLens";
 import { IdentityWorldCoin } from "./IdentityWorldCoin";
+
+import dynamic from "next/dynamic";
+
+const IdentityLens = dynamic(
+  () => import("./IdentityLens").then((mod) => mod.IdentityLens),
+  { ssr: false }
+);
 
 export const Identity = () => {
   return (
--- components/IdentityLens.tsx
+++ components/IdentityLens.tsx
@@ -1,5 +1,38 @@
 // TODO: https://docs.lens.xyz/docs/get-default-profile
 
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+
 export const IdentityLens = () => {
-  return <div className="bg-green-300 font-light">@shunkakinoki.lens</div>;
+  const { address } = useAccount();
+  const [lensHandle, setLensHandle] = useState();
+
+  useEffect(() => {
+    if (address) {
+      console.log(address);
+      fetch("https://api.lens.dev", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `
+            query DefaultProfile {
+              defaultProfile(request: { ethereumAddress: "${address}"}) {
+                handle
+              }
+            }
+          `,
+          variables: {
+            now: new Date().toISOString(),
+          },
+        }),
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          setLensHandle(result?.data?.defaultProfile?.handle);
+        });
+    }
+  }, [address]);
+  return <div className="bg-green-300 font-light">{lensHandle}</div>;
 };
