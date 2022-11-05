3ab1918804c2f382cfd3ba1b380913b063c088a1
Shun Kakinoki <shunkakinoki@gmail.com>
2022-11-04 22:03:11 -0700

    feat: 🎸 ini home dir app

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <head />
       <body>{children}</body>
     </html>
