4bfd9d63a049c5e1a73215605ef9c4a925f7d4e0
Shun Kakinoki <shunkakinoki@gmail.com>
2022-11-05 12:07:02 -0700

    feat: 🎸 ini eslint and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   tabWidth: 2,
   singleQuote: false,
+  overrides: [
+    {
+      files: "*.yaml",
+      options: {
+        bracketSpacing: false,
+        singleQuote: true,
+        printWidth: 300,
+      },
+    },
+  ],
 };
