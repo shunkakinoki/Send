0a40884d43260aeb81422a40369b72c02aa55de7
Shun Kakinoki <shunkakinoki@gmail.com>
2022-11-05 01:49:30 -0700

    feat: 🎸 ini client rendering

--- components/IdentityWorldCoin.tsx
+++ components/IdentityWorldCoin.tsx
@@ -1,14 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { WorldIDWidget } from "@worldcoin/id";
 
 export const IdentityWorldCoin = () => {
-  return (
-    <WorldIDWidget
-      actionId={process.env.NEXT_PUBLIC_WORLDCOIN_ID as string}
-      signal="my_signal"
-      enableTelemetry
-      onSuccess={(verificationResponse) => console.log(verificationResponse)}
-      onError={(error) => console.error(error)}
-      debug={true}
-    />
-  );
+  const [isRendered, setIsRendered] = useState(false);
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
+
+  if (isRendered) {
+    return (
+      <WorldIDWidget
+        actionId={process.env.NEXT_PUBLIC_WORLDCOIN_ID as string}
+        signal="my_signal"
+        enableTelemetry
+        onSuccess={(verificationResponse) => console.log(verificationResponse)}
+        onError={(error) => console.error(error)}
+        debug={true}
+      />
+    );
+  }
+  return null;
 };
