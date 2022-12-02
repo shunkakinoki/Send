dad506f5ef265e7e702b5d49d53d1f13779cd680
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-01 23:17:48 -0800

    feat: ini polish

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Identity } from "../components/Identity";
 import { Token } from "../components/Token";
 import { TokenDialog } from "../components/TokenDialogue";
 import { useState } from "react";
-
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
 const Home = () => {
   const { tokens } = useTokens();
   const [dialogue, setDialogue] = useState({
@@ -28,7 +28,16 @@ const Home = () => {
       </Head>
 
       <main className="container flex w-full flex-1 flex-col items-center justify-center text-center">
+        <div className="mx-auto my-4 flex h-20 w-20 items-center justify-center rounded-full bg-blue-100">
+          <EnvelopeIcon
+            className="h-10 w-10 text-blue-600"
+            aria-hidden="true"
+          />
+        </div>
         <h1 className="mb-4 text-6xl font-bold text-blue-300">Send</h1>
+        <p className="mt-2 mb-8 text-lg leading-8 text-gray-600 sm:text-center">
+          Send ERC20s or ETH to a wallet address or ENS. That&apos;s it.
+        </p>
         <Identity />
         <div className="my-4 w-full">
           {tokens &&
