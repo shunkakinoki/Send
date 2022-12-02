ebdb12b7d17acd2f02f78b27fb9b657e9293265a
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-01 23:13:26 -0800

    ini refresh remember

--- components/TokenDialogue.tsx
+++ components/TokenDialogue.tsx
@@ -127,8 +127,6 @@ export const TokenDialog = ({
         className="relative z-10"
         initialFocus={cancelButtonRef}
         onClose={() => {
-          setAddress("0x");
-          setENS("");
           setDialogue({ ...dialogue, open: false });
         }}
       >
@@ -345,8 +343,6 @@ export const TokenDialog = ({
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
                     onClick={() => {
-                      setAddress("0x");
-                      setENS("");
                       setDialogue({ ...dialogue, open: false });
                     }}
                     ref={cancelButtonRef}
