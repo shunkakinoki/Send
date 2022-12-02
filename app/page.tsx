5f782ec2ea617ded53f85e9793faf1e79d6e59e8
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-01 20:00:15 -0800

    feat: ini dialogue

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ import Head from "next/head";
 import { useTokens } from "../src/hooks/useTokens";
 import { Identity } from "../components/Identity";
 import { Token } from "../components/Token";
+import { TokenDialog } from "../components/TokenDialogue";
+import { useState } from "react";
 
 const Home = () => {
   const { tokens } = useTokens();
+  const [dialogue, setDialogue] = useState({ open: false, name: "" });
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -14,15 +18,22 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+      <main className="container flex w-full flex-1 flex-col items-center justify-center text-center">
         <h1 className="mb-4 text-6xl font-bold text-blue-300">Sender</h1>
         <Identity />
         <div className="my-4 w-full">
           {tokens &&
             tokens.map((asset, index) => {
-              return <Token key={index} asset={asset} />;
+              return (
+                <Token key={index} asset={asset} setDialogue={setDialogue} />
+              );
             })}
         </div>
+        <TokenDialog
+          open={dialogue.open}
+          dialogue={dialogue}
+          setDialogue={setDialogue}
+        />
       </main>
     </div>
   );
--- components/Token.tsx
+++ components/Token.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import { NextImage } from "./NextImage";
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
 
 export const separateFloat = (x: number) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -19,10 +20,12 @@ export type ProfileBoardItemTokenProps = {
     type?: string;
     value?: number;
   };
+  setDialogue: ({ open, name }: { open: boolean; name: string }) => void;
 };
 
 export const Token: FC<ProfileBoardItemTokenProps> = ({
   asset: { name, amount, symbol, value, icon_url },
+  setDialogue,
 }) => {
   return (
     <tr className="flex w-full">
@@ -60,6 +63,18 @@ export const Token: FC<ProfileBoardItemTokenProps> = ({
         <p className="overflow-hidden text-ellipsis">
           ${value && separateFloat(Number(value.toFixed(2)))}
         </p>
+      </td>
+      <td className="border-contrast-lower text-contrast-medium flex w-1/4 flex-initial shrink-0 items-center justify-end border-b py-3 pr-4 sm:w-1/5 md:w-1/6">
+        <button
+          onClick={() => {
+            setDialogue({ open: true, name: name ?? "asd" });
+          }}
+          type="button"
+          className="inline-flex items-center rounded-full border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          <EnvelopeIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
+          Send
+        </button>
       </td>
     </tr>
   );
